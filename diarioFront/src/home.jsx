ec5b954a0d8e5ce8d67636eaec8c3cd6902c1461
he445 <103337053+he445@users.noreply.github.com>
2022-09-25 15:23:24 -0300

    firt comit

---
+++ diarioFront/src/home.jsx
@@ -0,0 +1,20 @@
+
+import './home.css'
+
+function home() {
+  
+return (
+    <div className="home">
+     <section><nav className='navbar'>
+        <ul>
+          <li>home</li>
+          <li>Poesias</li>
+          <li>Contos</li>
+          <li>Prosa</li>
+        </ul>
+        </nav></section>
+    </div>
+  )
+}
+
+export default home
