d39a710e988df806b84a0f09c1e9cbb873f31e32
he445 <103337053+he445@users.noreply.github.com>
2022-09-26 16:33:21 -0300

    modal and form

---
+++ diarioFront/src/assets/formModal/formModal.jsx
@@ -0,0 +1,8 @@
+import "./formModal.css"
+function form()
+{return(
+   <section>
+   <button type="create"> Create</button>
+   </section>
+)}
+export default form
--- diarioFront/src/assets/navbar/navBar.jsx
+++ diarioFront/src/assets/navbar/navBar.jsx
@@ -1,13 +1,51 @@
+import Modal from "react-modal";
+import { useState } from "react";
+import Form from "../formModal/formModal";
 import "./navBar.css"
-function navbar()
-{return(
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    width: "40rem",
+    height: "50rem",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: " rgba(0, 0, 0, 0.8)",
+    borderRadius: "15px",
+  },
+  overlay: {
+    background: "rgba(0, 0, 0, 0.4)",
+  },
+};
+
+Modal.setAppElement("#root");
+function navbar(create)
+{
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  function handleModal() {
+    setModalIsOpen(!modalIsOpen);
+  }
+  return(
     <section><nav className='navbar'>
-    <ul className="navBarUl">
+    <h1 className="navbarh1">Diario da Loucura</h1>
+    <button className="modal-button" onClick={handleModal}> create</button>
+    {/* <ul className="navBarUl">
       <li className="navBarLI">Home</li>
       <li className="navBarLI">Poesias</li>
       <li className="navBarLI">Contos</li>
       <li className="navBarLI">Prosa</li>
-    </ul>
-    </nav></section>
+    </ul> */}
+    </nav>
+    <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={handleModal}
+        style={customStyles}
+        contentLabel="form Create"
+      >
+        <Form create={create}  handleModal={handleModal} />
+      </Modal>
+    </section>
 )}
 export default navbar
