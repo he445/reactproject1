7f5fdb07e6267c5fd4d12963830d46be125f0686
he445 <103337053+he445@users.noreply.github.com>
2022-09-26 15:53:28 -0300

    featured add

--- diarioFront/src/assets/Featured/Featured.jsx
+++ diarioFront/src/assets/Featured/Featured.jsx
@@ -1,8 +1,27 @@
-import "./featured.css"
+import "./Featured.css";
+
 function featured()
 {return(
-    <section>
-        
+    <section className="feature">
+      <h1>Textos de Destaque</h1>
+      <ul className="feature">
+        <li className="textlist">
+            <h2 className="title">Titulo</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo animi unde, tempora magni ea laboriosam numquam, labore explicabo possimus vel iste quam non placeat pariatur doloremque totam dignissimos, rem sequi?</p>
+        <button className="textbtn">Ler mais</button>
+        </li>
+        <li className="textlist">
+            <h2 className="title">Titulo</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo animi unde, tempora magni ea laboriosam numquam, labore explicabo possimus vel iste quam non placeat pariatur doloremque totam dignissimos, rem sequi?</p>
+        <button className="textbtn">Ler mais</button>
+        </li>
+        <li className="textlist">
+            <h2 className="title">Titulo</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo animi unde, tempora magni ea laboriosam numquam, labore explicabo possimus vel iste quam non placeat pariatur doloremque totam dignissimos, rem sequi?</p>
+        <button className="textbtn">Ler mais</button>
+        </li>
+
+        </ul>  
     </section>
 )}
 export default featured
--- diarioFront/src/assets/navbar/navBar.jsx
+++ diarioFront/src/assets/navbar/navBar.jsx
@@ -2,11 +2,11 @@ import "./navBar.css"
 function navbar()
 {return(
     <section><nav className='navbar'>
-    <ul>
-      <li>Home</li>
-      <li>Poesias</li>
-      <li>Contos</li>
-      <li>Prosa</li>
+    <ul className="navBarUl">
+      <li className="navBarLI">Home</li>
+      <li className="navBarLI">Poesias</li>
+      <li className="navBarLI">Contos</li>
+      <li className="navBarLI">Prosa</li>
     </ul>
     </nav></section>
 )}
