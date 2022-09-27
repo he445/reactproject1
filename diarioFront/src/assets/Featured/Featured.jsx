0559e26955934f2b7adfb81a296aa8bd0788e9b7
he445 <103337053+he445@users.noreply.github.com>
2022-09-27 18:39:34 -0300

    bug fix

--- diarioFront/src/assets/Featured/Featured.jsx
+++ diarioFront/src/assets/Featured/Featured.jsx
@@ -3,8 +3,8 @@ import "./Featured.css";
 function featured()
 {return(
     <section className="feature">
-      <h1>Textos de Destaque</h1>
-      <ul className="feature">
+      <h1>Textos </h1>
+      <ul className="featureUL">
         <li className="textlist">
             <h2 className="title">Titulo</h2>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo animi unde, tempora magni ea laboriosam numquam, labore explicabo possimus vel iste quam non placeat pariatur doloremque totam dignissimos, rem sequi?</p>
